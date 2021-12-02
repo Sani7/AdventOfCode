1ff5dbababfcb4a9e21a8755ec46fad5d76c4580
Sander Speetjens <sander.speetjens@gmail.com>
2021-12-02 08:36:41 +0100

    Day 2
    Working part 1 (had trouble with strcmp, forgot that it returned 0 if equal)

---
+++ 02/02.1/main.cpp
@@ -0,0 +1,83 @@
+#include "main.hpp"
+
+int main(int argc, char **argv)
+{
+    FILE *fpin, *fpout;
+    /* 
+     * Use 1 or 2 cmd arguments as file name to use for input and output
+     * when argc is 1 then no arguments passed, 2 is one arg and so on
+    */
+    switch (argc)
+    {
+    case 1:
+        fpin = fopen("input", "r");
+        fpout = stdout;
+        break;
+    case 3:
+        fpout = fopen(argv[2], "w");
+        if (fpout == NULL)
+        {
+            fprintf(stderr, "Not able to write to file %s\n", argv[2]);
+            exit(-2);
+        }
+        fpin = fopen(argv[1], "r");
+        break;
+    case 2:
+        fpin = fopen(argv[1], "r");
+        fpout = stdout;
+        break;
+    default:
+        break;
+    }
+    if (fpin == NULL && argc > 1)
+    {
+        fprintf(stderr, "Not able to acces input file %s\n", argv[1]);
+        exit(-1);
+    }
+    else if (fpin == NULL)
+    {
+        fprintf(stderr, "Not able to acces input file test\n");
+        exit(-1);
+    }
+
+    int y = 0, z = 0;
+    char tmp[12];
+    int amount, again;
+
+    again = fscanf(fpin, "%s %d", tmp, &amount);
+    while (again > 0)
+    {
+        if (strcmp(tmp, "forward") == 0)
+            z += amount;
+        else if (strcmp(tmp, "down") == 0)
+            y += amount;
+        else if (strcmp(tmp, "up") == 0)
+            y -= amount;
+
+        fprintf(fpout, "%7s y=%d,z=%d\n",tmp, y, z);
+
+        again = fscanf(fpin, "%s%d", tmp, &amount);
+    }
+
+    fprintf(fpout, "result: y=%d, z=%d, total: %d\n", y, z, y * z);
+    fclose(fpin);
+    if (argc > 2)
+        fclose(fpout);
+    return 0;
+}
+
+int calcFileLength(FILE *fp)
+{
+    char buff[BUFSIZ];
+    int ret = 0;
+    char *tmp;
+
+    fseek(fp, 0, SEEK_SET);
+    tmp = fgets(buff, BUFSIZ, fp);
+    while (tmp != NULL)
+    {
+        ret++;
+        tmp = fgets(buff, BUFSIZ, fp);
+    }
+    return ret;
+}
---
+++ 02/02.1/main.hpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <ctype.h>
+
+#include "../../libs/quickSort/quickSort.hpp"
+
+int calcFileLength(FILE* fp);
